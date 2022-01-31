964c8cd4d9b5f5dabeca871f027583ce43494ad0
Marc <87535604+Marcoram0@users.noreply.github.com>
2022-01-31 14:28:50 +0100

    29.1.2021

--- index.js
+++ index.js
@@ -1,32 +1,24 @@
-
 // Intents, Import code, Require code
 const { Client, Intents } = require("discord.js")
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES,] });
 const config = require("./config.json");
-const chalk = require('chalk')
-
-
-
+const chalk = require('chalk');
+const fetch = require("node-fetch");
+const moment = require("moment"); require("moment-duration-format");;
+const duration = moment.duration(client.uptime).format(" H [hrs], m [mins]");
+const ora = require('ora')
 
 
 // Presence
 client.on('ready', function () {
 
+
     //Booting animation
-    const P = ['Booting \\', 'Booting |', 'Booting /', 'Booting -'];
-    let x = 0;
-    const loader = setInterval(() => {
-        process.stdout.write(chalk.cyan`\r${P[x++]}`);
-        x %= P.length;
-    }, 100);
-
+    const spinne = ora().start();
+    spinne.color = 'red';
+    spinne.spinner = "aesthetic"
     setTimeout(() => {
-        clearInterval(loader);
-        console.clear();
-    }, 4999);
-
-
-    setTimeout(() => {
+        spinne.stop();
         console.log(chalk.green`
         
 ░██╗░░░░░░░██╗░█████╗░██████╗░██╗░░░░░██████╗░██████╗░░█████╗░████████╗
@@ -38,24 +30,38 @@ client.on('ready', function () {
 
 `)
     }, 5000);
-
-
     // Count client users
     let users = 0;
     client.guilds.cache.forEach(guild => users = users + guild.memberCount);
-
+    // Log message
     setTimeout(() => {
-        console.log(chalk.red(`${client.user.username}`, chalk.blue('is ready on'), chalk.red(`${client.guilds.cache.size} Guilds`), chalk.blue('and helping out'), chalk.red(`${users} Members`)));
-    }, 5500)
+        console.log(chalk.red(`${client.user.username}`, chalk.blue('is ready on'), chalk.red(`${client.guilds.cache.size} Guilds`), chalk.blue('and helping out'), chalk.red(`${users} Members`
+
+        ))
+        );
+    }, 5250)
+
 });
-
-
-
-
-
-
-
 
 
 // Login
 client.login(config.token)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
